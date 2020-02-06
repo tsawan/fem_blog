5ac44961d696b6928aacdcd2526fa7c6d35dbf8e
Tahir Awan <tahir@Tahirs-MacBook-Air.local>
2020-02-06 21:56:42 +0300

    added about page and favicon

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const About = () => (
+  <>
+    <h1>About Me</h1>
+    <p>My personal website with gatsby and react.</p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
+
+export default About;
