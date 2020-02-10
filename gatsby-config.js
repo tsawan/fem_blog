8d08921effd99a236865985294758caa33c75001
Tahir Awan <tahir.awan@gmail.com>
2020-02-10 21:50:48 +0300

    added a new hook use-posts, changed pages layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: 'My personal blog',
+    title: 'Frontend Masters Gatsby Workshop',
     description:
-      'built with Gatsby',
+      'A site we built together during a full-day Frontend Masters Gatsby workshop!',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 const About = () => (
-  <>
+  <Layout>
     <h1>About Me</h1>
     <p>My personal website with gatsby and react.</p>
     <Link to="/">&larr; back to home</Link>
-  </>
+   </Layout>
 );
 
 export default About;
